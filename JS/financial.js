60a47bdbd6fe0f95af9b4f957ba9e4bdc645acbb
JRussell94 <johnrussell9412@yahoo.com>
2023-08-02 21:59:27 -0500

    Created initial js file for financial

---
+++ JS/financial.js
@@ -0,0 +1,11 @@
+/* placement code so I could do use CSS to change result look */
+document.getElementById("result1").value = 72;
+document.getElementById("result2").value = 8.49 + "%";
+document.getElementById("result3").value = "$" + 2000;
+document.getElementById("result4").value = "$" + 395;
+
+
+
+
+
+
