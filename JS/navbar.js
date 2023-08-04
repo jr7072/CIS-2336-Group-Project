8fcd33ad675bcf69b326317d2237c292236196af
JRussell94 <johnrussell9412@yahoo.com>
2023-08-04 14:15:56 -0500

    Changed 'Accessories' to 'Products' on nav bar

--- JS/navbar.js
+++ JS/navbar.js
@@ -8,7 +8,7 @@ const navBar = () => {
           <li><a href="#">Car 1</a></li>
           <li><a href="#">Car 2</a></li>
           <li><a href="#">Car 3</a></li>
-          <li><a href="./Misc_accessories.html">Accessories</a></li>
+          <li><a href="./Misc_accessories.html">Products</a></li>
           <li><a href="./Services.html">Packages</a></li>
           <li><a href="./Financials.html">Finances</a></li>
           <li><a href="./About_us.html">About us</a></li>
