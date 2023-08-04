aeb4768081d6b7054faf5953020e609d11f1635d
okonwudiachi <97698064+okonwudiachi@users.noreply.github.com>
2023-08-04 01:15:05 -0500

    setting my branch equal to dev for further development

---
+++ JS/financial.js
@@ -0,0 +1,27 @@
+/* placement code so I could do use CSS to change result look */
+document.getElementById("result1").value = 72;
+document.getElementById("result2").value = 8.49 + "%";
+document.getElementById("result3").value = "$" + 2000;
+document.getElementById("result4").value = "$" + 395;
+
+
+
+
+/* "selected car", "selected package", "Total SRP", "Picture of car" */
+var a = document.getElementById("car-selection").value;
+var b = document.getElementById("package-selection").value;
+var c = document.getElementById("total-msrp").value;
+var d = document.getElementById("car-image").value;
+
+/* user input boxes */
+var e = document.getElementById("selected-credit-score").value;
+var f = document.getElementById("selected-cash-down").value;
+var g = document.getElementById("selected-term-length").value;
+var h = document.getElementById("estimated-apr-display1").value;
+var i = document.getElementById("trade-in-value").value;
+
+/* display boxes */
+var j = document.getElementById("result1").value;
+var k = document.getElementById("result2").value;
+var l = document.getElementById("result3").value;
+var m = document.getElementById("result4").value;
---
+++ JS/navbar.js
@@ -0,0 +1,20 @@
+const navBar = () => {
+
+  return (
+    `
+      <img src="../Images/4h.png" id="img1">
+      <ul>
+          <li><a href="./Home.html">Home</a></li>
+          <li><a href="#">Car 1</a></li>
+          <li><a href="#">Car 2</a></li>
+          <li><a href="#">Car 3</a></li>
+          <li><a href="./Misc_accessories.html">Accessories</a></li>
+          <li><a href="./Services.html">Packages</a></li>
+          <li><a href="./Financials.html">Finances</a></li>
+          <li><a href="./About_us.html">About us</a></li>
+      </ul> 
+    `
+  )
+}
+
+document.getElementById("nav-bar").innerHTML = navBar();
