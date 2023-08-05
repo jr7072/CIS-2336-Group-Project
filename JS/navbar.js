95ddd51a98b5cbba6f92ffe6d9c8d5dda03e78c7
okonwudiachi <97698064+okonwudiachi@users.noreply.github.com>
2023-08-05 16:02:10 -0500

    page finished and ready to be pushed

--- JS/navbar.js
+++ JS/navbar.js
@@ -5,9 +5,9 @@ const navBar = () => {
       <img src="../Images/4h.png" id="img1">
       <ul>
           <li><a href="./Home.html">Home</a></li>
-          <li><a href="#">Car 1</a></li>
-          <li><a href="#">Car 2</a></li>
-          <li><a href="#">Car 3</a></li>
+          <li><a href="./Car1.html">Car 1</a></li>
+          <li><a href="./Car2.html">Car 2</a></li>
+          <li><a href="./Car3.html">Car 3</a></li>
           <li><a href="./Misc_accessories.html">Accessories</a></li>
           <li><a href="./Services.html">Packages</a></li>
           <li><a href="./Financials.html">Finances</a></li>
