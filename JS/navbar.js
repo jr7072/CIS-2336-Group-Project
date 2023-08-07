a0ae5c20aeda23865b22443cf2f3c843a9e852ec
osa711 <79777974+osa711@users.noreply.github.com>
2023-08-07 04:39:55 -0500

    I added pages for the cars

--- JS/navbar.js
+++ JS/navbar.js
@@ -5,9 +5,9 @@ const navBar = () => {
       <img src="../Images/4h.png" id="img1">
       <ul>
           <li><a href="./Home.html">Home</a></li>
-          <li><a href="#">Car 1</a></li>
-          <li><a href="#">Car 2</a></li>
-          <li><a href="#">Car 3</a></li>
+          <li><a href="../HTML/p1mclaren.html">P1 Mclaren</a></li>
+          <li><a href="../HTML/Pagani_Huayra.html">Pagani Huayra</a></li>
+          <li><a href="../HTML/bmw_i8.html">Bmw i8</a></li>
           <li><a href="./Misc_accessories.html">Products</a></li>
           <li><a href="./Services.html">Packages</a></li>
           <li><a href="./Financials.html">Finances</a></li>
