3dbfdb09cf54854d86886b9ea736be74bcd931a8
Chris3999 <christopher.onyebuchico@yahoo.com>
2023-08-07 20:37:01 -0500

    Home page car video update

--- JS/navbar.js
+++ JS/navbar.js
@@ -5,9 +5,9 @@ const navBar = () => {
       <img src="../Images/4h.png" id="img1">
       <ul>
           <li><a href="./Home.html">Home</a></li>
-          <li><a href="#">Car 1</a></li>
-          <li><a href="#">Car 2</a></li>
-          <li><a href="#">Car 3</a></li>
+          <li><a href="#">Pagani</a></li>
+          <li><a href="#">BMW I8</a></li>
+          <li><a href="#">Mclaren</a></li>
           <li><a href="./Misc_accessories.html">Accessories</a></li>
           <li><a href="./Services.html">Packages</a></li>
           <li><a href="./Financials.html">Finances</a></li>
